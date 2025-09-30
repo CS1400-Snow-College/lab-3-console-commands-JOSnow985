e9a9e245dc8d8caf76efe5ee0d4aeae8b2ce4fe8
JOSnow985 <jaden.olvera@students.snow.edu>
2025-09-30 10:57:00 -0600

    checkpoint 2: greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// Jaden, 9/30/25, Lab 5 - Mastermind
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Mastermind!");
+Console.WriteLine("A game where you guess what our string is!");
+Console.WriteLine("The string will be 5 characters long!");
--- obj/Debug/net8.0/lab-3-console-commands-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-3-console-commands-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e742105d750c267db8daf08815ec814e28aa939b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddec2fca149381f03ee047b5ca2e89474ed3508e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
