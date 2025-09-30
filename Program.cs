f9856bb10fc8c45df98dceaabde3153bd567eb87
JOSnow985 <jaden.olvera@students.snow.edu>
2025-09-30 16:53:04 -0600

    implemented redrawing instructions

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿// Jaden, 9/30/25, Lab 5 - Mastermind
-Console.WriteLine("Welcome to Mastermind!");
-Console.WriteLine("A game where you guess what our string is!");
-Console.WriteLine("The string will be 5 characters long!");
 string hardCodedSecret = "bdefa";
 string playerGuess = "";
 int attemptNumber = 0;
@@ -11,12 +8,25 @@ bool playerGuessAccepted = false;
 
 do
 {
-    Console.WriteLine("Guess what the secret is! It can contain characters a through g, no repeats!");
-    Console.WriteLine("This is how many attempts you've made: " + attemptNumber);
-
-    //input validating
+    //Collecting user guess
     while (playerGuessAccepted == false)
     {
+        //Instructions
+        Console.Clear();
+        Console.WriteLine("Welcome to Mastermind!");
+        Console.WriteLine("A game where you guess what our string is!");
+        Console.WriteLine("The string will be 5 characters long!");
+        Console.WriteLine("Guess what the secret is! It can contain characters a through g, no repeats!");
+        if (attemptNumber > 0)
+        {
+            Console.WriteLine("This is how many attempts you've made: " + attemptNumber);
+            correctCharacters -= correctPositions;
+            Console.WriteLine("- " + correctPositions + " in the right position");
+            Console.WriteLine("- " + correctCharacters + " in the wrong position");
+        }
+        correctPositions = 0;
+        correctCharacters = 0;
+
         int errorValue = 0;
         playerGuess = Console.ReadLine();
 
@@ -44,6 +54,7 @@ do
                 else instancesofLetter = 0;
             }
         }
+        //check if letters exceed max
         if (errorValue == 0)
         {
             char rangeEnd = 'g';
@@ -54,16 +65,20 @@ do
         switch (errorValue)
         {
             case 1:
-                Console.WriteLine($"The input was null! Try again with a string of {hardCodedSecret.Length} characters!");
+                Console.WriteLine($"Your input was null! Try again with {hardCodedSecret.Length} characters!");
+                Thread.Sleep(4500);
                 break;
             case 2:
-                Console.WriteLine($"Looks like your guess wasn't the correct length, try again with a string of exactly {hardCodedSecret.Length} characters!");
+                Console.WriteLine($"Your guess wasn't the correct length, try again with exactly {hardCodedSecret.Length} characters!");
+                Thread.Sleep(4500);
                 break;
             case 3:
-                Console.WriteLine("Your guess had duplicate characters in it, our secret has no duplicates so only use unique letters!");
+                Console.WriteLine("Your guess had duplicate characters in it, try again with unique characters!");
+                Thread.Sleep(4500);
                 break;
             case 4:
                 Console.WriteLine("Your guess had a charcter that isn't in the same range as our secret, try again with characters between a and g!");
+                Thread.Sleep(4500);
                 break;
             default:
                 playerGuessAccepted = true;
@@ -84,13 +99,7 @@ do
             }
         }
     }
-    correctCharacters -= correctPositions;
-    Console.WriteLine("- " + correctPositions + " in the right position");
-    Console.WriteLine("- " + correctCharacters + " in the wrong position");
-    correctPositions = 0;
-    correctCharacters = 0;
-    Console.WriteLine();
     attemptNumber++;
     playerGuessAccepted = false;
-} while (hardCodedSecret != playerGuess);
+} while (playerGuess != hardCodedSecret);
 Console.WriteLine("It only took you " + attemptNumber + " attempts, nice!");
